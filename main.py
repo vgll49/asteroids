02a8d53a03a05e48159c5dd959a264fb14e8dfef
Aki-cmd <valentin.gill@ges.thm.de>
2024-10-21 19:59:15 +0200

    draw player shape

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            return
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,1))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
