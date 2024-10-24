4cf80d0fb7e71e416839416e7e1e68525769754f
Aki-cmd <valentin.gill@ges.thm.de>
2024-10-24 18:18:14 +0200

    add shots and collision

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+from circleshape import CircleShape
 
 
 def main():
@@ -8,10 +12,19 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
 
+
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
+
+    Shot.containers = (shots, drawable, updatable)
+    AsteroidField.containers = updatable
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    field = AsteroidField()
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
@@ -25,6 +38,18 @@ def main():
         for obj in updatable:
             obj.update(dt)
 
+        for obj in asteroids:
+            if obj.collides_with(player):
+                print("Game over!")
+                return
+            
+            for shot in shots:
+                if shot.collides_with(obj):
+                    shot.kill()
+                    obj.kill()
+
+                
+                
         screen.fill("black")
 
         for obj in drawable:
