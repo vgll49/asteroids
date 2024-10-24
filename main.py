e37ae3ceded86c27211cbf3fb46117b4cb6e0827
Aki-cmd <valentin.gill@ges.thm.de>
2024-10-24 16:34:18 +0200

    add player movement

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        player.update(dt)
         screen.fill("black")
         player.draw(screen)
+
         pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
